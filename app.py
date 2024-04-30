ff56e6f685331894e61a6e6528bb4fce8d081d02
Ditin Agrawal <113216643+ditinagrawal@users.noreply.github.com>
2024-04-30 19:14:06 +0000

    ui fix

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.set_page_config("ProbeAI")
+    st.header("Chat with PDF")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
