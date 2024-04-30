5279102568833e8b54e38a1153cd2556330d6818
Ditin Agrawal <113216643+ditinagrawal@users.noreply.github.com>
2024-04-30 19:25:08 +0000

    ui fix

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("ProbeAI")
-    st.header("Probe AI")
-    st.write("### Let your PDF speak for you")
+    st.header("Probe AI 🚀")
+    st.write("#### Let your PDF speak for you")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
@@ -95,8 +95,8 @@ def main():
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click the Button", accept_multiple_files=True)
+        if st.button("Let's Go"):
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
